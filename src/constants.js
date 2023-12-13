f75e4b2a3b59524034d2e52a8477f911529e8490
abishek <dhakalavishek53@gmail.com>
2023-12-12 23:37:05 -0500

    frontend url update

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 const localhostUrl = "http://localhost:8080"
 const renderUrl = "https://sellnow-backend.onrender.com"
 
-const serverUrl = (process.env.ENV === 'production') ? renderUrl: localhostUrl;
+// const serverUrl = (process.env.ENV === 'production') ? renderUrl: localhostUrl;
+const serverUrl = "https://sellnow-backend.onrender.com"
 
 export {serverUrl}
